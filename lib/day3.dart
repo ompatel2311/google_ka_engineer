3028d14897136415f9615d72e45af1d1cd05fc8f
Om Patel <op23112007@gmail.com>
2026-04-27 09:18:17 +0530

    day 3: User input ane null safety shikhyo | Double calculator banavyo

---
+++ lib/day3.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Day 3 - List ane Loop
+  List<String> skills = ["HTML", "CSS", "Dart", "Git"];
+  
+  print("Mari Day 3 ni Skills:");
+  for (var skill in skills) {
+    print("✓ $skill");
+  }
+  
+  print("Day 3 Complete. LAGSE TIME, AAVSE MAJA 🚁");
+}
